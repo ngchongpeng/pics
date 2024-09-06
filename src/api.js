359620d403e17fdd53bbc0fb46e7e344d9620fd9
ngchongpeng <ngchongpeng@hotmail.com>
2024-09-07 02:44:27 +0800

    commit changes.

--- src/api.js
+++ src/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
 	const response = await axios.get('https://api.unsplash.com/search/photos', {
 		headers: {
 			Authorization: 'Client-ID aDI_dYPD_HN0Y6aek0v-0IL5hkskAvoREmWCtPZD4dA'
 		},
 		params: {
-			query: 'cars'
+			query: term,
 		}
 	});
-
-	console.log(response);
 
 	return response;
 };
